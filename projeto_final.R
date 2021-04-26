56c87ec996873906022e360b4602bc3bd9a31dff
cadosanjos <60987620+cadosanjos@users.noreply.github.com>
2021-04-26 20:12:02 -0300

    Encoding file and named graphics code

--- projeto_final.R
+++ projeto_final.R
@@ -5,48 +5,48 @@ library("readxl")
 library("ggplot2")
 library("plotly")
 
-pop <- read.csv("./projeto_final.csv")
+pop <- read.csv("./projeto_final.csv", encoding = "UTF-8")
 View(pop)
 
-#1-d
+# Idade média da população
 idade_media = pop %>% summarise(mean(idade, na.rm = TRUE))
 View(idade_media) #35.0405
 
-#1-e
+# Salário médio da população
 salario_medio = pop %>% summarise(mean(salario, na.rm = TRUE))
 View(salario_medio) #11659.72
 
 
-#1-f
+# Idade da pessoa mais velha
 pessoa_velha = pop %>% summarise(max(idade, na.rm = TRUE))
 View(pessoa_velha)
 
 
-#1-g
+# Idade da pessoa mais nova
 pessoa_nova = pop %>% summarise(min(idade, na.rm = TRUE))
 View(pessoa_nova)
 
 
-#1-h
+# Quantidade de mulheres
 qtd_mulheres = pop %>% 
   group_by(sexo) %>% filter(sexo == "F") %>% summarise(qtd = n())
 View(qtd_mulheres) #5004
 
 
-#1-h
+# Quantidade de homens
 qtd_homens = pop %>% 
   group_by(sexo) %>% filter(sexo == "M") %>% summarise(qtd = n())
 View(qtd_homens) #4996
 
 
-#2
+# Gráfico Histograma - Quantidade de pessoas x Idade
 pop %>%
   ggplot()+
   geom_histogram(aes(x=idade), color="black", fill="pink")+ #binwidth = 2
   labs(x="Idade", y="Quantidade de pessoas")
 
 
-#3
+# Gráfico de Pontos Iterativo - IMC x Escolaridade x Fumante
 graf <- pop %>% 
   filter(!is.na(fumante.f)) %>%
   ggplot() +
@@ -57,35 +57,35 @@ graf <- pop %>%
 ggplotly(graf)
 
 
-#4a - IMC x Salário x Sexo
+# Gráfico de Pontos - IMC x Salário x Sexo
 pop %>%
   ggplot()+
   geom_point(aes(x=imc, y=salario, color=sexo))+
   labs(x="IMC", y="Salário", color="Sexo")
 
 
-#4b - Peso x Idade x Sexo
+# Gráfico de Linha - Peso x Idade x Sexo
 pop %>%
   ggplot()+
   geom_line(aes(x=peso, y=idade, color=sexo))+
   labs(x="Peso", y="Idade", color="Sexo")
 
 
-#4c - Peso x Filhos x Salário
+# Gráfico de Pontos - Peso x Filhos x Salário
 pop %>%
   ggplot()+
   geom_point(aes(x=peso, y=salario, color=filhos))+
   labs(x="Peso", y="Salário", color="Filhos")
 
 
-#4d - Altura x Peso
+# Gráfico de Linha- Altura x Peso
 pop %>%
   ggplot()+
   geom_line(aes(x=altura, y=peso))+
   labs(x="Altura", y="Peso")
 
 
-#4e - Peso x Fumante
+# Gráfico de Pontos Iterativo - Peso x Fumante
 grafic <- pop %>% 
   ggplot() +
   geom_point(mapping = aes(x=peso, y=fumante.f))+
